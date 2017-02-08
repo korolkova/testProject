5f44086c7c07071cb9cfbaa95443b1aec69b16e4
ikorolkova <ikorolkovad@gmail.com>
2017-02-08 15:45:03 +0300

    deleted excess dependency and changed UserRepository extends

--- src/main/java/ru/test/jotter/UserRepository.java
+++ src/main/java/ru/test/jotter/UserRepository.java
@@ -1,16 +1,19 @@
 package ru.test.jotter;
 
-import java.util.List;
+//import java.util.List;
 
 import ru.test.jotter.model.User;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+//import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "user", path = "user")
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+//import org.springframework.data.web.PagedResourcesAssembler;
 
-	List<User> findByName(@Param("name")String name);
+@RepositoryRestResource(/*collectionResourceRel = "user",*/ path = "user")
+public interface UserRepository extends CrudRepository/*PagingAndSortingRepository*/<User, Long> {
+
+	//List<User> findByName(@Param("name")String name);
 	User findOne(@Param("id") long id);
 }
