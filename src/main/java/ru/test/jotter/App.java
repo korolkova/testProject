2d4e7ac457cdbe932274e01f3238900321de54bf
ikorolkova <ikorolkovad@gmail.com>
2017-02-09 11:33:17 +0300

    removed excess dependency, added UserRepository

--- src/main/java/ru/test/jotter/App.java
+++ src/main/java/ru/test/jotter/App.java
@@ -1,22 +1,34 @@
 package ru.test.jotter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import ru.test.jotter.repository.UserRepository;
 
 @RestController
 @EnableAutoConfiguration
 public class App{
 
-	//private static final Logger logger = LoggerFactory.getLogger(App.class);
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
 		
-	/*@RequestMapping("/user")
-    User userById(@RequestParam("id") int id) {
-		UserServiceImpl userService=new UserServiceImpl();
-		return userService.getUserById(id);
-    }*/
 	
-  	
+	@Autowired()
+	private UserRepository userRepository;
+	
+	
+	@RequestMapping(value="remove")
+	public void removeUser(@RequestParam("id") Integer id){
+		
+		this.userRepository.delete((long)id);
+		logger.info("removed user with id = "+id);
+	}
+		
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);       
     }
