2b5527dd95d7aca942e90416d7d8220da7808ad4
ikorolkova <ikorolkovad@gmail.com>
2017-02-10 12:26:41 +0300

    removed method 'removeUser' from App class, added UserController with method 'removeUser'

--- src/main/java/ru/test/jotter/UserController.java
+++ src/main/java/ru/test/jotter/UserController.java
@@ -1,25 +1,20 @@
 package ru.test.jotter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ru.test.jotter.repository.UserRepository;
 
 @RestController
-@EnableAutoConfiguration
-@ComponentScan
-public class App{
-
-	/*private static final Logger logger = LoggerFactory.getLogger(App.class);
-		
+public class UserController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+
 	@Autowired()
 	private UserRepository userRepository;
 	
@@ -29,9 +24,6 @@ public class App{
 		
 		this.userRepository.delete((long)id);
 		logger.info("removed user with id = "+id);
-	}*/
-		
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(App.class, args);       
-    }
+	}
+
 }
